95d65ba7c894a13b1f584e0ee0f286073f17d8b5
mar-fullstack <mar@fullstack.ph>
2019-06-13 10:51:38 +0800

    Initial Commit

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 
 const BlogPage = ({data}) => (
   <Layout>
-    <h1>Laytest Posts</h1>
+    <h1>Latest Posts</h1>
     {data.allMarkdownRemark.edges.map(post => (
       <div key={post.node.id}>
         <h3>{post.node.frontmatter.title}</h3>
